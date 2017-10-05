d6ffec20cc9e2a32783a88b6a506fd3d6f8870a4
Vlatko Trajkov <trajkovvlatko@gmail.com>
2017-10-05 23:10:46 +0200

    Styles and js for code mirror

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+// Brunch automatically concatenates all files in your
+// watched paths. Those paths can be configured at
+// config.paths.watched in "brunch-config.js".
+//
+// However, those files will only be executed if
+// explicitly imported. The only exception are files
+// in vendor, which are never wrapped in imports and
+// therefore are always executed.
+
+// Import dependencies
+//
+// If you no longer want to use a dependency, remember
+// to also remove its path from "config.paths.watched".
+import "phoenix_html"
+
+// Import local files
+//
+// Local files can be imported directly using relative
+// paths "./socket" or full ones "web/static/js/socket".
+
+// import socket from "./socket"
+
+
+var fields = document.querySelectorAll("textarea");
+if (fields.length > 0) {
+  for (var i = 0; i < fields.length; i++) {
+    var field = fields[i];
+    var codeMirrorOptions = {
+      lineNumbers: true,
+      tabSize: 2,
+    }
+    if (field.className.indexOf("readonly") > -1) {
+      codeMirrorOptions.readOnly = true;
+    }
+    CodeMirror.fromTextArea(field, codeMirrorOptions);
+  }
+}
