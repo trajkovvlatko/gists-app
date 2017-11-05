6772d913707efe5be34c0e6b50b54fc92cdf61e1
Vlatko Trajkov <trajkovvlatko@gmail.com>
2017-11-05 15:14:52 +0100

    Getting ready for heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :gists_app, GistsAppWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  # url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "fathomless-oasis-39544.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,6 +62,8 @@ config :logger, level: :info
 #     config :gists_app, GistsAppWeb.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :gists_app, GistsApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
